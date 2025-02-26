99750b8d9dadaa7e0f857984b456a78dd8ae3101
zakiamansyah <zakiamansyah2014@gmail.com>
2025-02-26 16:14:35 +0700

    Enhance ProcessComment job to utilize Redis for caching and error handling

--- app/Jobs/ProcessComment.php
+++ app/Jobs/ProcessComment.php
@@ -6,10 +6,11 @@ use App\Models\Comments;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Log;
 
 class ProcessComment implements ShouldQueue
 {
@@ -24,13 +25,32 @@ class ProcessComment implements ShouldQueue
 
     public function handle()
     {
-        $comment = Comments::find($this->commentId);
+        try {
+            Redis::ping();
 
-        if ($comment) {
-        $comment->save();
-            Log::info("✅ Comment {$this->commentId} processed by Redis queue.");
-        } else {
-            Log::error("❌ Failed to process comment ID: {$this->commentId}");
+            $uniqueKey = 'comment_2025_{$id_from_database}_{$primary_key_database_if_exist}';
+
+            $getData = Redis::get($uniqueKey);
+
+            if (!$getData) {
+                $comment = Comments::find($this->commentId);
+
+                if ($comment) {
+                    $comment = Comments::find($this->commentId);
+                    Redis::set($uniqueKey);
+
+                    $comment->save();
+
+                    Log::info("✅ Comment {$this->commentId} processed by Redis queue.");
+                } else {
+                    Log::error("❌ Comment ID: {$this->commentId} not found in the database.");
+                }
+            } else {
+                Log::info("✅ Comment {$this->commentId} retrieved from Redis cache.");
+            }
+        } catch (\Exception $e) {
+            Log::error("❌ Redis connection failed: " . $e->getMessage());
         }
     }
+
 }
